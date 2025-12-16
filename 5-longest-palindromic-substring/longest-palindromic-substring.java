b33d7e5234a4acbfbc8d4d1c10bb7cb166c9ad12
Sanjay <sanjaygs2406@gmail.com>
2025-12-16 18:56:06 +0530

    Time: 14 ms (90.66%) | Memory: 43.7 MB (51.12%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -5,8 +5,8 @@ class Solution {
         int start = 0, end = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expand(s, i, i);     // odd length
-            int len2 = expand(s, i, i + 1); // even length
+            int len1 = expand(s, i, i);     
+            int len2 = expand(s, i, i + 1); 
             int len = Math.max(len1, len2);
 
             if (len > end - start) {
